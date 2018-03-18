0c8055fac835ce0c3cc6ddfdfae16c1b5c377429
shaoziyang <shaoziyang@126.com>
2018-03-18 22:17:03 +0800

    fixed pxt.json 'name'

--- main.ts
+++ main.ts
@@ -8,7 +8,7 @@
  * Four Digit Display
  */
 //% weight=100 color=#64C800 icon="日"
-namespace FourDigitDisplay {
+namespace TM1650 {
 
     let COMMAND_I2C_ADDRESS = 0x24
     let DISPLAY_I2C_ADDRESS = 0x34
